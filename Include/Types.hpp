e740c5fde58cffa0e24add1d6cac0902198334ad
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-02-22 00:23:31 +0900

    feat: adding default integer types

---
+++ Include/Types.hpp
@@ -0,0 +1,26 @@
+//
+// Created by arthur on 22/02/2023.
+//
+
+#ifndef CONCERTOCORE_INCLUDE_TYPES_HPP_
+#define CONCERTOCORE_INCLUDE_TYPES_HPP_
+
+#include <cstdint>
+
+namespace Concerto
+{
+	using Int8 = std::uint8_t;
+	using Int16 = std::uint16_t;
+	using Int32 = std::uint32_t;
+	using Int64 = std::uint64_t;
+
+	using UInt8 = std::int8_t;
+	using UInt16 = std::int16_t;
+	using UInt32 = std::int32_t;
+	using UInt64 = std::int64_t;
+
+	using Byte = std::int8_t;
+	using UByte = std::uint8_t;
+}
+
+#endif //CONCERTOCORE_INCLUDE_TYPES_HPP_
