7f48d6542d5acd4d02205cc5f0669cb7d9eb0402
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-02-25 19:40:34 +0900

    fix: wrong types

--- Include/Concerto/Core/Types.hpp
+++ Include/Concerto/Core/Types.hpp
@@ -28,15 +28,15 @@ namespace Concerto
 	#define CONCERTO_PUBLIC_API CONCERTO_IMPORT
 #endif
 
-	using Int8 = std::uint8_t;
-	using Int16 = std::uint16_t;
-	using Int32 = std::uint32_t;
-	using Int64 = std::uint64_t;
+	using Int8 = std::int8_t;
+	using Int16 = std::int16_t;
+	using Int32 = std::int32_t;
+	using Int64 = std::int64_t;
 
-	using UInt8 = std::int8_t;
-	using UInt16 = std::int16_t;
-	using UInt32 = std::int32_t;
-	using UInt64 = std::int64_t;
+	using UInt8 = std::uint8_t;
+	using UInt16 = std::uint16_t;
+	using UInt32 = std::uint32_t;
+	using UInt64 = std::uint64_t;
 
 	using Byte = std::int8_t;
 	using UByte = std::uint8_t;
