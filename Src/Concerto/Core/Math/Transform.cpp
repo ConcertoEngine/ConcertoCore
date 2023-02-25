8ca6981dcbc3a2aa3d7cabd4528d97f46b824d41
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-02-25 19:25:40 +0900

    feat: adding new functions to Transform class

--- Src/Concerto/Core/Math/Transform.cpp
+++ Src/Concerto/Core/Math/Transform.cpp
@@ -4,19 +4,58 @@
 
 #include "Concerto/Core/Math/Transform.hpp"
 
-void Concerto::Math::Transform::translate(float x, float y, float z)
+namespace Concerto::Math
 {
-	Location.X() += x;
-	Location.Y() += y;
-	Location.Z() += z;
-}
 
-void Concerto::Math::Transform::translate(const Concerto::Math::Vector3f& vec)
-{
-	translate(vec.X(), vec.Y(), vec.Z());
-}
+	Transform::Transform(const Vector3f& location, const Quaternionf& rotation, const Vector3f& scale) :
+		_location(location), _rotation(rotation), _scale(scale)
+	{
+	}
 
-bool Concerto::Math::Transform::operator==(const Concerto::Math::Transform& other) const
-{
-	return Location == other.Location && Rotation == other.Rotation && Scale == other.Scale;
+	void Transform::Translate(float x, float y, float z)
+	{
+		_location.X() += x;
+		_location.Y() += y;
+		_location.Z() += z;
+	}
+
+	void Transform::Translate(const Vector3f& vec)
+	{
+		Translate(vec.X(), vec.Y(), vec.Z());
+	}
+
+	void Transform::Rotate(const Quaternionf& rotation)
+	{
+		_rotation *= rotation;
+	}
+
+	void Transform::Scale(const Vector3f& scale)
+	{
+		_scale *= scale;
+	}
+
+	const Vector3f& Transform::GetLocation() const
+	{
+		return _location;
+	}
+
+	const Quaternionf& Transform::GetRotation() const
+	{
+		return _rotation;
+	}
+
+	const Vector3f& Transform::GetScale() const
+	{
+		return _scale;
+	}
+
+	bool Transform::operator==(const Transform& other) const
+	{
+		return _location == other._location && _rotation == other._rotation && _scale == other._scale;
+	}
+
+	bool Transform::operator!=(const Transform& other) const
+	{
+		return (*this == other);
+	}
 }
