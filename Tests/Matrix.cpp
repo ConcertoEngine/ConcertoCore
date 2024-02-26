568e43aa2d9b27ac2eb1f7317d85184ea98b0e9d
Arthur Vasseur <arthur.vasseur@epitech.eu>
2024-02-26 10:44:31 +0100

    Matrix: promote operator() instead of GetElement

--- Tests/Matrix.cpp
+++ Tests/Matrix.cpp
@@ -42,15 +42,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m3 = m1 + m2;
-		ASSERT_EQ(2.f, m3.GetElement(0, 0));
-		ASSERT_EQ(4.f, m3.GetElement(0, 1));
-		ASSERT_EQ(6.f, m3.GetElement(0, 2));
-		ASSERT_EQ(8.f, m3.GetElement(1, 0));
-		ASSERT_EQ(10.f, m3.GetElement(1, 1));
-		ASSERT_EQ(12.f, m3.GetElement(1, 2));
-		ASSERT_EQ(14.f, m3.GetElement(2, 0));
-		ASSERT_EQ(16.f, m3.GetElement(2, 1));
-		ASSERT_EQ(18.f, m3.GetElement(2, 2));
+		ASSERT_EQ(2.f, m3(0, 0));
+		ASSERT_EQ(4.f, m3(0, 1));
+		ASSERT_EQ(6.f, m3(0, 2));
+		ASSERT_EQ(8.f, m3(1, 0));
+		ASSERT_EQ(10.f, m3(1, 1));
+		ASSERT_EQ(12.f, m3(1, 2));
+		ASSERT_EQ(14.f, m3(2, 0));
+		ASSERT_EQ(16.f, m3(2, 1));
+		ASSERT_EQ(18.f, m3(2, 2));
 	}
 
 	TEST(Matrix, operatorSub)
@@ -63,15 +63,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m3 = m1 - m2;
-		ASSERT_EQ(0.f, m3.GetElement(0, 0));
-		ASSERT_EQ(0.f, m3.GetElement(0, 1));
-		ASSERT_EQ(0.f, m3.GetElement(0, 2));
-		ASSERT_EQ(0.f, m3.GetElement(1, 0));
-		ASSERT_EQ(0.f, m3.GetElement(1, 1));
-		ASSERT_EQ(0.f, m3.GetElement(1, 2));
-		ASSERT_EQ(0.f, m3.GetElement(2, 0));
-		ASSERT_EQ(0.f, m3.GetElement(2, 1));
-		ASSERT_EQ(0.f, m3.GetElement(2, 2));
+		ASSERT_EQ(0.f, m3(0, 0));
+		ASSERT_EQ(0.f, m3(0, 1));
+		ASSERT_EQ(0.f, m3(0, 2));
+		ASSERT_EQ(0.f, m3(1, 0));
+		ASSERT_EQ(0.f, m3(1, 1));
+		ASSERT_EQ(0.f, m3(1, 2));
+		ASSERT_EQ(0.f, m3(2, 0));
+		ASSERT_EQ(0.f, m3(2, 1));
+		ASSERT_EQ(0.f, m3(2, 2));
 	}
 
 	TEST(Matrix, operatorMult)
@@ -79,19 +79,20 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 		Matrix<float, 3, 3> m1(1.f, 2.f, 3.f,
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
+
 		Matrix<float, 3, 3> m2(1.f, 2.f, 3.f,
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m3 = m1 * m2;
-		ASSERT_EQ(30.f, m3.GetElement(0, 0));
-		ASSERT_EQ(36.f, m3.GetElement(0, 1));
-		ASSERT_EQ(42.f, m3.GetElement(0, 2));
-		ASSERT_EQ(66.f, m3.GetElement(1, 0));
-		ASSERT_EQ(81.f, m3.GetElement(1, 1));
-		ASSERT_EQ(96.f, m3.GetElement(1, 2));
-		ASSERT_EQ(102.f, m3.GetElement(2, 0));
-		ASSERT_EQ(126.f, m3.GetElement(2, 1));
-		ASSERT_EQ(150.f, m3.GetElement(2, 2));
+		ASSERT_EQ(30.f, m3(0, 0));
+		ASSERT_EQ(36.f, m3(0, 1));
+		ASSERT_EQ(42.f, m3(0, 2));
+		ASSERT_EQ(66.f, m3(1, 0));
+		ASSERT_EQ(81.f, m3(1, 1));
+		ASSERT_EQ(96.f, m3(1, 2));
+		ASSERT_EQ(102.f, m3(2, 0));
+		ASSERT_EQ(126.f, m3(2, 1));
+		ASSERT_EQ(150.f, m3(2, 2));
 	}
 
 	TEST(Matrix, operatorDiv)
@@ -103,15 +104,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m3 = m1 / m2;
-		ASSERT_EQ(1.f, m3.GetElement(0, 0));
-		ASSERT_EQ(1.f, m3.GetElement(0, 1));
-		ASSERT_EQ(1.f, m3.GetElement(0, 2));
-		ASSERT_EQ(1.f, m3.GetElement(1, 0));
-		ASSERT_EQ(1.f, m3.GetElement(1, 1));
-		ASSERT_EQ(1.f, m3.GetElement(1, 2));
-		ASSERT_EQ(1.f, m3.GetElement(2, 0));
-		ASSERT_EQ(1.f, m3.GetElement(2, 1));
-		ASSERT_EQ(1.f, m3.GetElement(2, 2));
+		ASSERT_EQ(1.f, m3(0, 0));
+		ASSERT_EQ(1.f, m3(0, 1));
+		ASSERT_EQ(1.f, m3(0, 2));
+		ASSERT_EQ(1.f, m3(1, 0));
+		ASSERT_EQ(1.f, m3(1, 1));
+		ASSERT_EQ(1.f, m3(1, 2));
+		ASSERT_EQ(1.f, m3(2, 0));
+		ASSERT_EQ(1.f, m3(2, 1));
+		ASSERT_EQ(1.f, m3(2, 2));
 	}
 
 	TEST(Matrix, operatorAddScalar)
@@ -120,15 +121,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m2 = m1 + 1.f;
-		ASSERT_EQ(2.f, m2.GetElement(0, 0));
-		ASSERT_EQ(3.f, m2.GetElement(0, 1));
-		ASSERT_EQ(4.f, m2.GetElement(0, 2));
-		ASSERT_EQ(5.f, m2.GetElement(1, 0));
-		ASSERT_EQ(6.f, m2.GetElement(1, 1));
-		ASSERT_EQ(7.f, m2.GetElement(1, 2));
-		ASSERT_EQ(8.f, m2.GetElement(2, 0));
-		ASSERT_EQ(9.f, m2.GetElement(2, 1));
-		ASSERT_EQ(10.f, m2.GetElement(2, 2));
+		ASSERT_EQ(2.f, m2(0, 0));
+		ASSERT_EQ(3.f, m2(0, 1));
+		ASSERT_EQ(4.f, m2(0, 2));
+		ASSERT_EQ(5.f, m2(1, 0));
+		ASSERT_EQ(6.f, m2(1, 1));
+		ASSERT_EQ(7.f, m2(1, 2));
+		ASSERT_EQ(8.f, m2(2, 0));
+		ASSERT_EQ(9.f, m2(2, 1));
+		ASSERT_EQ(10.f, m2(2, 2));
 	}
 
 	TEST(Matrix, operatorSubScalar)
@@ -137,15 +138,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m2 = m1 - 1.f;
-		ASSERT_EQ(0.f, m2.GetElement(0, 0));
-		ASSERT_EQ(1.f, m2.GetElement(0, 1));
-		ASSERT_EQ(2.f, m2.GetElement(0, 2));
-		ASSERT_EQ(3.f, m2.GetElement(1, 0));
-		ASSERT_EQ(4.f, m2.GetElement(1, 1));
-		ASSERT_EQ(5.f, m2.GetElement(1, 2));
-		ASSERT_EQ(6.f, m2.GetElement(2, 0));
-		ASSERT_EQ(7.f, m2.GetElement(2, 1));
-		ASSERT_EQ(8.f, m2.GetElement(2, 2));
+		ASSERT_EQ(0.f, m2(0, 0));
+		ASSERT_EQ(1.f, m2(0, 1));
+		ASSERT_EQ(2.f, m2(0, 2));
+		ASSERT_EQ(3.f, m2(1, 0));
+		ASSERT_EQ(4.f, m2(1, 1));
+		ASSERT_EQ(5.f, m2(1, 2));
+		ASSERT_EQ(6.f, m2(2, 0));
+		ASSERT_EQ(7.f, m2(2, 1));
+		ASSERT_EQ(8.f, m2(2, 2));
 	}
 
 	TEST(Matrix, operatorMultScalar)
@@ -154,15 +155,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m2 = m1 * 2.f;
-		ASSERT_EQ(2.f, m2.GetElement(0, 0));
-		ASSERT_EQ(4.f, m2.GetElement(0, 1));
-		ASSERT_EQ(6.f, m2.GetElement(0, 2));
-		ASSERT_EQ(8.f, m2.GetElement(1, 0));
-		ASSERT_EQ(10.f, m2.GetElement(1, 1));
-		ASSERT_EQ(12.f, m2.GetElement(1, 2));
-		ASSERT_EQ(14.f, m2.GetElement(2, 0));
-		ASSERT_EQ(16.f, m2.GetElement(2, 1));
-		ASSERT_EQ(18.f, m2.GetElement(2, 2));
+		ASSERT_EQ(2.f, m2(0, 0));
+		ASSERT_EQ(4.f, m2(0, 1));
+		ASSERT_EQ(6.f, m2(0, 2));
+		ASSERT_EQ(8.f, m2(1, 0));
+		ASSERT_EQ(10.f, m2(1, 1));
+		ASSERT_EQ(12.f, m2(1, 2));
+		ASSERT_EQ(14.f, m2(2, 0));
+		ASSERT_EQ(16.f, m2(2, 1));
+		ASSERT_EQ(18.f, m2(2, 2));
 	}
 
 	TEST(Matrix, operatorDivScalar)
@@ -171,15 +172,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		Matrix<float, 3, 3> m2 = m1 / 2.f;
-		ASSERT_EQ(0.5f, m2.GetElement(0, 0));
-		ASSERT_EQ(1.f, m2.GetElement(0, 1));
-		ASSERT_EQ(1.5f, m2.GetElement(0, 2));
-		ASSERT_EQ(2.f, m2.GetElement(1, 0));
-		ASSERT_EQ(2.5f, m2.GetElement(1, 1));
-		ASSERT_EQ(3.f, m2.GetElement(1, 2));
-		ASSERT_EQ(3.5f, m2.GetElement(2, 0));
-		ASSERT_EQ(4.f, m2.GetElement(2, 1));
-		ASSERT_EQ(4.5f, m2.GetElement(2, 2));
+		ASSERT_EQ(0.5f, m2(0, 0));
+		ASSERT_EQ(1.f, m2(0, 1));
+		ASSERT_EQ(1.5f, m2(0, 2));
+		ASSERT_EQ(2.f, m2(1, 0));
+		ASSERT_EQ(2.5f, m2(1, 1));
+		ASSERT_EQ(3.f, m2(1, 2));
+		ASSERT_EQ(3.5f, m2(2, 0));
+		ASSERT_EQ(4.f, m2(2, 1));
+		ASSERT_EQ(4.5f, m2(2, 2));
 	}
 
 	TEST(Matrix, operatorAddAssign)
@@ -191,15 +192,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 += m2;
-		ASSERT_EQ(2.f, m1.GetElement(0, 0));
-		ASSERT_EQ(4.f, m1.GetElement(0, 1));
-		ASSERT_EQ(6.f, m1.GetElement(0, 2));
-		ASSERT_EQ(8.f, m1.GetElement(1, 0));
-		ASSERT_EQ(10.f, m1.GetElement(1, 1));
-		ASSERT_EQ(12.f, m1.GetElement(1, 2));
-		ASSERT_EQ(14.f, m1.GetElement(2, 0));
-		ASSERT_EQ(16.f, m1.GetElement(2, 1));
-		ASSERT_EQ(18.f, m1.GetElement(2, 2));
+		ASSERT_EQ(2.f, m1(0, 0));
+		ASSERT_EQ(4.f, m1(0, 1));
+		ASSERT_EQ(6.f, m1(0, 2));
+		ASSERT_EQ(8.f, m1(1, 0));
+		ASSERT_EQ(10.f, m1(1, 1));
+		ASSERT_EQ(12.f, m1(1, 2));
+		ASSERT_EQ(14.f, m1(2, 0));
+		ASSERT_EQ(16.f, m1(2, 1));
+		ASSERT_EQ(18.f, m1(2, 2));
 	}
 
 	TEST(Matrix, operatorSubAssign)
@@ -211,15 +212,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 -= m2;
-		ASSERT_EQ(0.f, m1.GetElement(0, 0));
-		ASSERT_EQ(0.f, m1.GetElement(0, 1));
-		ASSERT_EQ(0.f, m1.GetElement(0, 2));
-		ASSERT_EQ(0.f, m1.GetElement(1, 0));
-		ASSERT_EQ(0.f, m1.GetElement(1, 1));
-		ASSERT_EQ(0.f, m1.GetElement(1, 2));
-		ASSERT_EQ(0.f, m1.GetElement(2, 0));
-		ASSERT_EQ(0.f, m1.GetElement(2, 1));
-		ASSERT_EQ(0.f, m1.GetElement(2, 2));
+		ASSERT_EQ(0.f, m1(0, 0));
+		ASSERT_EQ(0.f, m1(0, 1));
+		ASSERT_EQ(0.f, m1(0, 2));
+		ASSERT_EQ(0.f, m1(1, 0));
+		ASSERT_EQ(0.f, m1(1, 1));
+		ASSERT_EQ(0.f, m1(1, 2));
+		ASSERT_EQ(0.f, m1(2, 0));
+		ASSERT_EQ(0.f, m1(2, 1));
+		ASSERT_EQ(0.f, m1(2, 2));
 	}
 
 	TEST(Matrix, operatorMultAssign)
@@ -231,15 +232,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 *= m2;
-		ASSERT_EQ(1.f, m1.GetElement(0, 0));
-		ASSERT_EQ(4.f, m1.GetElement(0, 1));
-		ASSERT_EQ(9.f, m1.GetElement(0, 2));
-		ASSERT_EQ(16.f, m1.GetElement(1, 0));
-		ASSERT_EQ(25.f, m1.GetElement(1, 1));
-		ASSERT_EQ(36.f, m1.GetElement(1, 2));
-		ASSERT_EQ(49.f, m1.GetElement(2, 0));
-		ASSERT_EQ(64.f, m1.GetElement(2, 1));
-		ASSERT_EQ(81.f, m1.GetElement(2, 2));
+		ASSERT_EQ(1.f, m1(0, 0));
+		ASSERT_EQ(4.f, m1(0, 1));
+		ASSERT_EQ(9.f, m1(0, 2));
+		ASSERT_EQ(16.f, m1(1, 0));
+		ASSERT_EQ(25.f, m1(1, 1));
+		ASSERT_EQ(36.f, m1(1, 2));
+		ASSERT_EQ(49.f, m1(2, 0));
+		ASSERT_EQ(64.f, m1(2, 1));
+		ASSERT_EQ(81.f, m1(2, 2));
 	}
 
 	TEST(Matrix, operatorDivAssign)
@@ -251,15 +252,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 /= m2;
-		ASSERT_EQ(1.f, m1.GetElement(0, 0));
-		ASSERT_EQ(1.f, m1.GetElement(0, 1));
-		ASSERT_EQ(1.f, m1.GetElement(0, 2));
-		ASSERT_EQ(1.f, m1.GetElement(1, 0));
-		ASSERT_EQ(1.f, m1.GetElement(1, 1));
-		ASSERT_EQ(1.f, m1.GetElement(1, 2));
-		ASSERT_EQ(1.f, m1.GetElement(2, 0));
-		ASSERT_EQ(1.f, m1.GetElement(2, 1));
-		ASSERT_EQ(1.f, m1.GetElement(2, 2));
+		ASSERT_EQ(1.f, m1(0, 0));
+		ASSERT_EQ(1.f, m1(0, 1));
+		ASSERT_EQ(1.f, m1(0, 2));
+		ASSERT_EQ(1.f, m1(1, 0));
+		ASSERT_EQ(1.f, m1(1, 1));
+		ASSERT_EQ(1.f, m1(1, 2));
+		ASSERT_EQ(1.f, m1(2, 0));
+		ASSERT_EQ(1.f, m1(2, 1));
+		ASSERT_EQ(1.f, m1(2, 2));
 	}
 
 	TEST(Matrix, operatorAddAssignScalar)
@@ -268,15 +269,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 += 1.f;
-		ASSERT_EQ(2.f, m1.GetElement(0, 0));
-		ASSERT_EQ(3.f, m1.GetElement(0, 1));
-		ASSERT_EQ(4.f, m1.GetElement(0, 2));
-		ASSERT_EQ(5.f, m1.GetElement(1, 0));
-		ASSERT_EQ(6.f, m1.GetElement(1, 1));
-		ASSERT_EQ(7.f, m1.GetElement(1, 2));
-		ASSERT_EQ(8.f, m1.GetElement(2, 0));
-		ASSERT_EQ(9.f, m1.GetElement(2, 1));
-		ASSERT_EQ(10.f, m1.GetElement(2, 2));
+		ASSERT_EQ(2.f, m1(0, 0));
+		ASSERT_EQ(3.f, m1(0, 1));
+		ASSERT_EQ(4.f, m1(0, 2));
+		ASSERT_EQ(5.f, m1(1, 0));
+		ASSERT_EQ(6.f, m1(1, 1));
+		ASSERT_EQ(7.f, m1(1, 2));
+		ASSERT_EQ(8.f, m1(2, 0));
+		ASSERT_EQ(9.f, m1(2, 1));
+		ASSERT_EQ(10.f, m1(2, 2));
 	}
 
 	TEST(Matrix, operatorSubAssignScalar)
@@ -285,15 +286,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 -= 1.f;
-		ASSERT_EQ(0.f, m1.GetElement(0, 0));
-		ASSERT_EQ(1.f, m1.GetElement(0, 1));
-		ASSERT_EQ(2.f, m1.GetElement(0, 2));
-		ASSERT_EQ(3.f, m1.GetElement(1, 0));
-		ASSERT_EQ(4.f, m1.GetElement(1, 1));
-		ASSERT_EQ(5.f, m1.GetElement(1, 2));
-		ASSERT_EQ(6.f, m1.GetElement(2, 0));
-		ASSERT_EQ(7.f, m1.GetElement(2, 1));
-		ASSERT_EQ(8.f, m1.GetElement(2, 2));
+		ASSERT_EQ(0.f, m1(0, 0));
+		ASSERT_EQ(1.f, m1(0, 1));
+		ASSERT_EQ(2.f, m1(0, 2));
+		ASSERT_EQ(3.f, m1(1, 0));
+		ASSERT_EQ(4.f, m1(1, 1));
+		ASSERT_EQ(5.f, m1(1, 2));
+		ASSERT_EQ(6.f, m1(2, 0));
+		ASSERT_EQ(7.f, m1(2, 1));
+		ASSERT_EQ(8.f, m1(2, 2));
 	}
 
 	TEST(Matrix, operatorMultAssignScalar)
@@ -302,15 +303,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 *= 2.f;
-		ASSERT_EQ(2.f, m1.GetElement(0, 0));
-		ASSERT_EQ(4.f, m1.GetElement(0, 1));
-		ASSERT_EQ(6.f, m1.GetElement(0, 2));
-		ASSERT_EQ(8.f, m1.GetElement(1, 0));
-		ASSERT_EQ(10.f, m1.GetElement(1, 1));
-		ASSERT_EQ(12.f, m1.GetElement(1, 2));
-		ASSERT_EQ(14.f, m1.GetElement(2, 0));
-		ASSERT_EQ(16.f, m1.GetElement(2, 1));
-		ASSERT_EQ(18.f, m1.GetElement(2, 2));
+		ASSERT_EQ(2.f, m1(0, 0));
+		ASSERT_EQ(4.f, m1(0, 1));
+		ASSERT_EQ(6.f, m1(0, 2));
+		ASSERT_EQ(8.f, m1(1, 0));
+		ASSERT_EQ(10.f, m1(1, 1));
+		ASSERT_EQ(12.f, m1(1, 2));
+		ASSERT_EQ(14.f, m1(2, 0));
+		ASSERT_EQ(16.f, m1(2, 1));
+		ASSERT_EQ(18.f, m1(2, 2));
 	}
 
 	TEST(Matrix, operatorDivAssignScalar)
@@ -319,15 +320,15 @@ namespace CONCERTO_ANONYMOUS_NAMESPACE
 							   4.f, 5.f, 6.f,
 							   7.f, 8.f, 9.f);
 		m1 /= 2.f;
-		ASSERT_EQ(0.5f, m1.GetElement(0, 0));
-		ASSERT_EQ(1.f, m1.GetElement(0, 1));
-		ASSERT_EQ(1.5f, m1.GetElement(0, 2));
-		ASSERT_EQ(2.f, m1.GetElement(1, 0));
-		ASSERT_EQ(2.5f, m1.GetElement(1, 1));
-		ASSERT_EQ(3.f, m1.GetElement(1, 2));
-		ASSERT_EQ(3.5f, m1.GetElement(2, 0));
-		ASSERT_EQ(4.f, m1.GetElement(2, 1));
-		ASSERT_EQ(4.5f, m1.GetElement(2, 2));
+		ASSERT_EQ(0.5f, m1(0, 0));
+		ASSERT_EQ(1.f, m1(0, 1));
+		ASSERT_EQ(1.5f, m1(0, 2));
+		ASSERT_EQ(2.f, m1(1, 0));
+		ASSERT_EQ(2.5f, m1(1, 1));
+		ASSERT_EQ(3.f, m1(1, 2));
+		ASSERT_EQ(3.5f, m1(2, 0));
+		ASSERT_EQ(4.f, m1(2, 1));
+		ASSERT_EQ(4.5f, m1(2, 2));
 	}
 
 	//TEST(Matrix, Translate)
