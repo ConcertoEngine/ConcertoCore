6ef5a0486877a3c160ff89453ac5aaf022c50501
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-05-01 22:14:10 +0200

    feat: add DeferredExit class with basic functionality and tests

---
+++ Tests/DeferredExit.cpp
@@ -0,0 +1,25 @@
+//
+// Created by arthur on 23/02/2023.
+//
+
+#include <gtest/gtest.h>
+#include "Concerto/Core/DeferredExit.hpp"
+
+#include "Concerto/Core/Types.hpp"
+
+namespace CCT_ANONYMOUS_NAMESPACE
+{
+	using namespace cct;
+
+	TEST(DeferredExit, General)
+	{
+		Int32 i = 0;
+		{
+			DeferredExit _([&]()
+			{
+				i = 42;
+			});
+		}
+		ASSERT_EQ(i, 42);
+	}
+}// namespace CCT_ANONYMOUS_NAMESPACE
